eac8b3546233311d3d94b134b54b4b88536dbec0
Tasnim <tasnimalbrashdi@gmail.com>
2024-11-25 15:10:10 +0400

    Add TranscationRepository

--- Repositories/TranscationRepository.cs
+++ Repositories/TranscationRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ServicesLab1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,36 @@ namespace ServicesLab1.Repositories
             _context = context;
         }
 
+        public IEnumerable<Transaction> GetAllUsers()
+        {
+            return _context.Transactions.ToList();
+        }
 
+        public Transaction GetTranscationById(int TId)
+        {
+            return _context.Transactions.Include(t => t.BankAccounts).FirstOrDefault(t =>t.TId == TId);
+        }
+
+        public void AddTranscation(Transaction transcation)
+        {
+            _context.Transactions.Add(transcation);
+            _context.SaveChanges();
+        }
+
+        public void UpdateTransaction(Transaction transaction)
+        {
+            _context.Transactions.Update(transaction);
+            _context.SaveChanges();
+        }
+
+        public void DeleteTransaction(int Id)
+        {
+            var transaction = _context.Transactions.Find(Id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+                _context.SaveChanges();
+            }
+        }
     }
 }
